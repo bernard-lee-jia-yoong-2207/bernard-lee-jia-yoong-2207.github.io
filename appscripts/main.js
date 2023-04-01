79052c6e51be35adf5016d316ad4963908c0ad35
bernard-lee-jia-yoong-2207 <125462183+bernard-lee-jia-yoong-2207@users.noreply.github.com>
2023-04-01 21:18:48 +0800

    Week 11 updates
    Updated level 1 - maze game

--- appscripts/main.js
+++ appscripts/main.js
@@ -7,12 +7,12 @@ function openGame(evt, tabName) {
     defaultContent[i].style.display = "none";
   }
 
-  tabcontent = document.getElementsByClassName("gameContent");
+  tabcontent = document.getElementsByClassName("gameContainer");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  tablinks = document.getElementsByClassName("gameButton");
+  tablinks = document.getElementsByClassName("tab");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
     tablinks[i].style.display = "none";
@@ -24,14 +24,14 @@ function openGame(evt, tabName) {
 
 function popup(popupNumber) {
   var popup = document.getElementById(popupNumber);
-  popup.classList.toggle("show");
+  popup.className += " show";
 }
 
 setTimeout(popup, 1000, "popup1");
 
 function closepopup(popupNumber) {
   var popup = document.getElementById(popupNumber);
-  popup.classList.toggle("close");
+  popup.className += " close";
 }
 
 document.getElementById("beginButton").addEventListener("click", function () {
@@ -40,6 +40,7 @@ document.getElementById("beginButton").addEventListener("click", function () {
   setTimeout(closepopup, 4000, "popup2");
   setTimeout(popup, 5500, "popup3");
   setTimeout(enablegame, 8500, "Game_1");
+  setTimeout(closepopup, 9000, "popup3")
 });
 
 function enablegame(game_number) {
@@ -51,7 +52,17 @@ function disablegame(game_number) {
 }
 
 document.getElementById("Game_1").addEventListener("click", function () {
-  closepopup("popup3");
+  setTimeout(popup, 500, "mazePopup");
+})
+
+document.getElementById("mazeEndButton").addEventListener("click", function () {
+  backHome();
+  disablegame("Game_1");
+  enablegame("Game_2");
+})
+
+document.getElementById("mazeTAButton").addEventListener("click", function () {
+  console.log("try again");
 })
 
 function backHome() {
@@ -62,12 +73,12 @@ function backHome() {
     defaultContent[i].style.display = "block";
   }
 
-  tabcontent = document.getElementsByClassName("gameContent");
+  tabcontent = document.getElementsByClassName("gameContainer");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  tablinks = document.getElementsByClassName("gameButton");
+  tablinks = document.getElementsByClassName("tab");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.display = "block";
   }
@@ -76,20 +87,20 @@ function backHome() {
 //this part is just for milestone submission, please change!!!
 document.getElementById("game1resetButton").addEventListener("click", function () {
   backHome();
-  disablegame("Game_1");
-  enablegame("Game_2");
 })
 
 document.getElementById("game2resetButton").addEventListener("click", function () {
   backHome();
-  document.getElementById("Game_1").className += " active";
+  //document.getElementById("Game_1").className += " active";
   disablegame("Game_2");
   enablegame("Game_3");
 })
 
 document.getElementById("game3resetButton").addEventListener("click", function () {
   backHome();
-  document.getElementById("Game_1").className += " active";
-  document.getElementById("Game_2").className += " active";
+  //document.getElementById("Game_1").className += " active";
+  //document.getElementById("Game_2").className += " active";
   disablegame("Game_3");
 })
+
+let gameCounter = ["fail", "fail", "fail"];
---
+++ appscripts/maze.js
@@ -0,0 +1,352 @@
+function closepopup(popupNumber) {
+    var popup = document.getElementById(popupNumber);
+    popup.classList.toggle("close");
+};
+
+document.getElementById("mazeStartButton").addEventListener("click", function () {
+    closepopup("mazePopup");
+    this.className += " active";
+});
+
+//this code references js-maze-game-1 by nevkatz on GitHub with some modification
+
+//define details for the current level
+let levels = [];
+
+levels[0] = {
+    map: [
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], //0 represents a floor, 1 represents a well
+        [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+        [1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0],
+        [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+        [0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+        [0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0],
+        [0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+        [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+        [1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+        [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+        [0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+        [1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
+        [0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+        [1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
+        [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+        [0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+        [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+    ],
+    player: {
+        x: 2,
+        y: 28
+    },
+    goal: {
+/*         x: 26,
+        y: 0 */
+        x:3,
+        y:26
+    },
+};
+
+//build object constructor for game, with parameters id of game container and the level array
+function Game(id, level) {
+    //capture the game conteiner from HTML
+    this.el = document.getElementById(id);
+
+    //define the two types of tile that will be used on the map - floor and wall
+    this.tileTypes = ['floor', 'wall'];
+
+    //value that will be used to define the width and height of each tile
+    this.tileDim = 18;
+
+    this.map = level.map;
+
+    //copying the player and goal coordinates without changing the value
+    this.player = { ...level.player };
+    this.goal = { ...level.goal };
+
+    this.player.el = null;
+}
+
+//object method to insert tile in HTML
+Game.prototype.populateMap = function () {
+    let tiles = document.getElementById('tiles');
+    for (var y = 0; y < this.map.length; ++y) {
+        //loop function as long as y coordinate is less than the number of rows
+        for (var x = 0; x < this.map[y].length; ++x) {
+            //loop function as long as x coordinate is less than the number of tiles per row
+            let tileCode = this.map[y][x];
+            let tileType = this.tileTypes[tileCode];
+            let tile = this.createEl(x, y, tileType);
+            //add tiles to the 'tiles' layer in HTML
+            tiles.appendChild(tile);
+        }
+    }
+}
+
+//object method to create new tile elements in HTML
+Game.prototype.createEl = function (x, y, type) {
+    let el = document.createElement('div');
+
+    //define the tile type
+    el.className = type;
+
+
+    el.style.width = el.style.height = this.tileDim + 'px';
+    el.style.left = x * this.tileDim + 'px';
+    el.style.top = y * this.tileDim + 'px';
+    return el;
+}
+
+//object method to adjust and correct the size of the map
+Game.prototype.sizeUp = function () {
+    let map = this.el.querySelector('.mazeMap');
+    //defining the dimensions of the entire map
+    map.style.height = this.map.length * this.tileDim + 'px';
+    //uniform map so width can be as long as the number of items in the first row
+    map.style.width = this.map[0].length * this.tileDim + 'px';
+}
+
+//object method to place player and goal
+Game.prototype.placeSprite = function (type) {
+    let x = this[type].x;
+    let y = this[type].y;
+
+    //create an element using the coordinates of the player/goal as defined in the level
+    let sprite = this.createEl(x, y, type);
+    sprite.id = type;
+
+    //make the player/goal round
+    sprite.style.borderRadius = this.tileDim + 'px';
+
+    //add player/goal in HTML
+    let layer = this.el.querySelector('#sprites');
+    layer.appendChild(sprite);
+    return sprite;
+}
+
+//object method to move the player
+Game.prototype.movePlayer = function (event) {
+    //make sure that keys other than the arrow keys do not work
+    event.preventDefault();
+    if (event.keyCode < 37 || event.keyCode > 40) {
+        return;
+    }
+
+    //check if the player has reached the goal
+    else if (this.player.y == this.goal.y &&
+        this.player.x == this.goal.x) {
+            return;
+    }
+
+    //use switch case to determine the action for each arrow key
+    switch (event.keyCode) {
+        case 37:
+            this.moveLeft();
+            break;
+
+        case 38:
+            this.moveUp();
+            break;
+
+        case 39:
+            this.moveRight();
+            break;
+
+        case 40:
+            this.moveDown();
+            break;
+    }
+}
+
+//object method to define the action for every key
+Game.prototype.moveUp = function () {
+    //wiggle if player is on the top row
+    if (this.player.y == 0) {
+        this.collide();
+        return;
+    }
+
+    //access the next tile (same x coordinate, y coordinate minus one)
+    //if the next tile is a wall, wiggle
+    let nextTile = this.map[this.player.y - 1][this.player.x];
+    if (nextTile == 1) {
+        this.collide();
+        return;
+    }
+
+    //otherwise move player up one tile
+    this.player.y -= 1;
+    this.updateVert();
+}
+
+Game.prototype.moveDown = function () {
+    //wiggle if player is on the bottom row
+    //the number of rows may be changed in the level so this.map.length-1 is used instead of an absolute y
+    if (this.player.y == this.map.length - 1) {
+        this.collide();
+        return;
+    }
+
+    //access the next tile (same x coordinate, y coordinate plus one)
+    //if the next tile is a wall, wiggle
+    let nextTile = this.map[this.player.y + 1][this.player.x];
+    if (nextTile == 1) {
+        this.collide();
+        return;
+    }
+
+    //otherwise move player down one tile
+    this.player.y += 1;
+    this.updateVert();
+}
+
+//update the top property of the player
+//this is used for both moving up and down
+Game.prototype.updateVert = function () {
+    this.player.el.style.top = this.player.y * this.tileDim + 'px';
+}
+
+Game.prototype.moveLeft = function () {
+    //wiggle if player is on the leftmost column
+    if (this.player.x == 0) {
+        this.collide();
+        return;
+    }
+
+    //access the next tile (x coordinate minus one, same y coordinate)
+    //if the next tile is a wall, wiggle
+    let nextTile = this.map[this.player.y][this.player.x - 1];
+    if (nextTile == 1) {
+        this.collide();
+        return;
+    }
+
+    //otherwise move player left one tile
+    this.player.x -= 1;
+    this.updateHoriz();
+}
+
+Game.prototype.moveRight = function () {
+    //wiggle if player is on the rightmost column
+    //the number of columns may be changed in the level so this.map[this.player.y].length - 1 is used instead of an absolute x
+    if (this.player.x == this.map[this.player.y].length - 1) {
+        this.collide();
+        return;
+    }
+
+    //access the next tile (x coordinate plus one, same y coordinate)
+    //if the next tile is a wall, wiggle
+    let nextTile = this.map[this.player.y][this.player.x + 1];
+    if (nextTile == 1) {
+        this.collide();
+        return;
+    }
+
+    //otherwise move player right one tile
+    this.player.x += 1;
+    this.updateHoriz();
+}
+
+//update the left property of the player
+//this is used for both moving left and right
+Game.prototype.updateHoriz = function () {
+    this.player.el.style.left = this.player.x * this.tileDim + 'px';
+}
+
+//add keyboard listenser for keyboard events
+Game.prototype.keyboardListener = function () {
+    document.addEventListener('keydown', event => {
+
+        //call the movePlayer method when arrow keys are pressed
+        this.movePlayer(event);
+
+        //check whether the player has reached the goal
+        this.checkGoal();
+    })
+}
+
+function popup(popupNumber) {
+    var popup = document.getElementById(popupNumber);
+    popup.className += " show";
+  }
+
+Game.prototype.checkGoal = function () {
+    //if the coordinates of the player is the same as the pre-defined coordinates of the goal
+    //set body's class to 'success'
+    let body = document.querySelector('body');
+    if (this.player.y == this.goal.y &&
+        this.player.x == this.goal.x) {
+        body.className = "success";
+        popup("mazePopupSuccess");
+    }
+
+    //otherwise body's class remain the same
+    else {
+        body.className = '';
+    }
+}
+
+//object method to define how the player wiggles
+Game.prototype.collide = function () {
+    this.player.el.className += ' collide';
+    let obj = this;
+    //this is so that we can still refer to the game object within the set time out function 
+    //otherwise 'this' will refer to the setTimeout function within setTimeout
+    window.setTimeout(function () {
+        obj.player.el.className = 'player';
+    }, 200);
+    return 0;
+}
+
+//function to run timer referncing Florin Pop's 'Simple Countdown Timer with JavaScript - Day 21' on YouTube
+const startingMin = 59/60;
+let time = startingMin * 60;
+const stoppingTime = 0;
+function updateCountdown() {
+    const timerEl = document.getElementById("timer");
+    const mins = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    timerEl.innerHTML = mins + ":" + seconds;
+
+
+    //stops the timer when the number of seconds reaches 0
+    if(time == stoppingTime) {
+        clearInterval(updateCountdown);
+        popup("mazePopupFail");
+        return
+    }
+
+    time--;
+
+    time = time < 0 ? 0 : time;    
+    console.log(time);
+}
+
+function init() {
+    let myGame = new Game('mazeGame', levels[0]);
+    myGame.populateMap();
+    myGame.sizeUp();
+    myGame.placeSprite('goal');
+    let playerSprite = myGame.placeSprite('player');
+    myGame.player.el = playerSprite;
+    let startButton = document.getElementById("mazeStartButton");
+    startButton.addEventListener("click", function () {
+        myGame.keyboardListener();
+        setInterval(updateCountdown, 1000);
+    })
+}
+
+init();
